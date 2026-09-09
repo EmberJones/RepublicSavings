05ff70577d503986afc251afa06174afa84d90a6
devillierscloete <166004164+devillierscloete@users.noreply.github.com>
2026-09-09 10:49:27 +0200

    basic UI fragment assignment
    fragments for UI screens created
    placeholder colours have been assignment in the colours script and simple theme layouts were made in the themes scripts.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,6 +32,9 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+    buildFeatures {
+        viewBinding = true
+    }
     configurations.configureEach {
         exclude(group = "com.intellij", module = "annotations")
     }
@@ -39,9 +42,14 @@ android {
 
 dependencies {
     implementation(libs.androidx.activity.ktx)
+    implementation(libs.androidx.annotation)
     implementation(libs.androidx.appcompat)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.livedata.ktx)
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
     implementation(libs.material)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.espresso.core)
