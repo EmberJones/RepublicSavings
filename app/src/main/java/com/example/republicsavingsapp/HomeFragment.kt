dff0af94f7afc054baff28d6d6badcac72c93642
Ember Willow Jones <st10450923@vgconnect.edu.za>
2026-09-17 14:25:17 +0200

    trying to fix vanishing screens

--- app/src/main/java/com/example/republicsavingsapp/HomeFragment.kt
+++ app/src/main/java/com/example/republicsavingsapp/HomeFragment.kt
@@ -1,4 +1,4 @@
-package com.example.republicsavingsapp.ui.home
+package com.example.republicsavingsapp
 
 import android.os.Bundle
 import android.view.View
@@ -6,9 +6,6 @@ import androidx.fragment.app.Fragment
 import com.example.republicsavingsapp.databinding.FragmentHomeBinding
 import java.util.Calendar
 import androidx.lifecycle.lifecycleScope
-import com.example.republicsavingsapp.CategoryTotal
-import com.example.republicsavingsapp.CurrentUser
-import com.example.republicsavingsapp.RepublicSavingsApp
 import com.example.republicsavingsapp.ui.categories.Category
 import kotlinx.coroutines.launch
 import java.util.Locale
